56fed532f6ef82d0ea83927bc6899b158ca6a8e3
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2018-12-27 19:02:18 -0600

    Add File Reader
    We want to be able to read a file. This adds a FileReader module that
    reads a given file.

---
+++ lib/pluralsight_tweet/file_reader.ex
@@ -0,0 +1,9 @@
+defmodule PluralsightTweet.FileReader do
+  def get_strings_to_tweet(path) do
+    File.read!(path)
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+    # |> Enum.filter( fn str -> String.length(str) <= 140 end)
+    |> Enum.filter(&String.length(&1) <= 140)
+  end
+end
