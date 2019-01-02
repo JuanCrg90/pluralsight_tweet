f21d1c7f5a32eea371ea45c31d69d620fd75331c
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2019-01-02 07:13:33 -0600

    Add Basic Unit Test

--- lib/pluralsight_tweet.ex
+++ lib/pluralsight_tweet.ex
@@ -1,18 +1,2 @@
 defmodule PluralsightTweet do
-  @moduledoc """
-  Documentation for PluralsightTweet.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PluralsightTweet.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
